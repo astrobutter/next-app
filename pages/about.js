c4c281860210d9aed4d40d2f47a66992c7533c97
Ayush <95138683+astrobutter@users.noreply.github.com>
2023-04-04 15:40:15 +0530

    mext app files

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (  
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name='keywords' content='ninjas' />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p> Esse commodo amet labore fugiat. Lorem sunt et irure culpa nostrud. Tempor velit ipsum aliqua nostrud est aliqua sunt ea ea incididunt. Incididunt non est proident occaecat eiusmod irure aliquip eiusmod officia. Ad elit dolor duis veniam mollit ullamco qui eiusmod sit occaecat elit aute. Excepteur voluptate Lorem aute laboris.</p>
+                <p> Esse commodo amet labore fugiat. Lorem sunt et irure culpa nostrud. Tempor velit ipsum aliqua nostrud est aliqua sunt ea ea incididunt. Incididunt non est proident occaecat eiusmod irure aliquip eiusmod officia. Ad elit dolor duis veniam mollit ullamco qui eiusmod sit occaecat elit aute. Excepteur voluptate Lorem aute laboris.</p>
+            </div>
+        </>
+    );
+}
+ 
+export default About;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,35 @@
+import styles from '../../styles/ninjas.module.css'
+import Head from 'next/head'
+import Link from 'next/link';
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data }
+  }
+}
+
+const Ninjas = ({ ninjas }) => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Listing</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1>All Ninjas</h1>
+        {ninjas.map(ninja => (
+          <Link href={"/ninjas/" + ninja.id} key={ninja.id}>
+            <div className={styles.single}>
+              <h3>{ ninja.name }</h3>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+}
+ 
+export default Ninjas;
